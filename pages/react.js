93ce29c308a57fb5c41319fd44f2a4e2726952bf
Iris van Ollefen <irisvanollefen@gmail.com>
2021-11-25 11:47:45 +0100

    Made subcategories show up

--- pages/react.js
+++ pages/react.js
@@ -12,12 +12,16 @@ query ReactPage {
   }) {
     id
     title
-    content {
-      value
-    }
     category {
       id
       name
+    }
+    subcategory {
+      id
+      name
+    }
+    content {
+      value
     }
     slug
     summary {
@@ -25,6 +29,8 @@ query ReactPage {
     }
   }
 }`
+
+
 
 export async function getStaticProps() {
   const data = await request({
@@ -36,14 +42,69 @@ export async function getStaticProps() {
 }
 
 export default function Reactpage({ data }) {
+
+  const ReactBasics = data.allPosts.filter((post) => post.subcategory.name === "React Basics" )
+  const ReactConcepts = data.allPosts.filter((post) => post.subcategory.name === "React Concepts" )
+  const ReactHooks = data.allPosts.filter((post) => post.subcategory.name === "React Hooks" )
+  const ReactDeepDive = data.allPosts.filter((post) => post.subcategory.name === "Deep Dive" )
+
   return (
     <Layout>
       <h2>{data.allPosts[0].category.name} Explained</h2>
-      {data.allPosts.map((post) => { 
+      <h3 className="subcat-title">{ReactBasics[0].subcategory.name}</h3>
+      {ReactBasics.map((post) => { 
         return (
           <article key={post.id}>
-            <h3>{post.title}</h3>
-            <StructuredText data={post.summary} />
+            <h4 className="post-title">{post.title}</h4>
+            <details> 
+              <summary>Summary</summary> 
+              <StructuredText data={post.summary} /> 
+            </details>
+            <Link href={`/react/${post.slug}`}>
+              <a className="more-link">Read More</a>
+            </Link>
+          </article>
+        )
+      })}
+      <h3 className="subcat-title">{ReactConcepts[0].subcategory.name}</h3>
+      {ReactConcepts.map((post) => { 
+        return (
+          <article key={post.id}>
+            <h4 className="post-title">{post.title}</h4>
+            <details> 
+              <summary>Summary</summary> 
+              <StructuredText data={post.summary} /> 
+            </details>
+            <Link href={`/react/${post.slug}`}>
+              <a className="more-link">Read More</a>
+            </Link>
+          </article>
+        )
+      })}
+      <h3 className="subcat-title">{ReactHooks[0].subcategory.name}</h3>
+      {ReactHooks.map((post) => { 
+        return (
+          <article key={post.id}>
+            <h4 className="post-title">{post.title}</h4>
+            <details> 
+              <summary>Summary</summary> 
+              <StructuredText data={post.summary} /> 
+            </details>
+            <Link href={`/react/${post.slug}`}>
+              <a className="more-link">Read More</a>
+            </Link>
+          </article>
+        )
+      })}
+      <h3 className="subcat-title">{ReactDeepDive[0].subcategory.name}</h3>
+      {ReactDeepDive.map((post) => { 
+        return (
+          <article key={post.id}>
+            <h4 className="post-title">{post.title}</h4>
+            <details> 
+              <summary>Summary</summary> 
+              <StructuredText data={post.summary} /> 
+            </details>
             <Link href={`/react/${post.slug}`}>
               <a className="more-link">Read More</a>
             </Link>
