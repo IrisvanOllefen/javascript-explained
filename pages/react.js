a6b0e476af83cef3922a86a839286d47e402954f
Iris van Ollefen <irisvanollefen@gmail.com>
2021-11-26 10:42:27 +0100

    Made subcategory order dynamic

--- pages/react.js
+++ pages/react.js
@@ -9,7 +9,7 @@ query ReactPage {
     category: {
       eq: 76436891
     }
-  }) {
+  }, orderBy: [postIndexNumber_ASC]) {
     id
     title
     category {
@@ -30,8 +30,6 @@ query ReactPage {
   }
 }`
 
-
-
 export async function getStaticProps() {
   const data = await request({
     query: REACT_QUERY
@@ -43,73 +41,35 @@ export async function getStaticProps() {
 
 export default function Reactpage({ data }) {
 
-  const ReactBasics = data.allPosts.filter((post) => post.subcategory.name === "React Basics" )
-  const ReactConcepts = data.allPosts.filter((post) => post.subcategory.name === "React Concepts" )
-  const ReactHooks = data.allPosts.filter((post) => post.subcategory.name === "React Hooks" )
-  const ReactDeepDive = data.allPosts.filter((post) => post.subcategory.name === "Deep Dive" )
+  const newArr1 = []
+  data.allPosts.forEach((post) => {
+    if (!newArr1.includes(post.subcategory.name)) {
+      newArr1.push(post.subcategory.name)
+    }
+    })
 
   return (
     <Layout>
       <h2>{data.allPosts[0].category.name} Explained</h2>
-      <h3 className="subcat-title">{ReactBasics[0].subcategory.name}</h3>
-      {ReactBasics.map((post) => { 
-        return (
-          <article key={post.id}>
-            <h4 className="post-title">{post.title}</h4>
-            <details> 
-              <summary>Summary</summary> 
-              <StructuredText data={post.summary} /> 
-            </details>
-            <Link href={`/react/${post.slug}`}>
-              <a className="more-link">Read More</a>
-            </Link>
-          </article>
-        )
-      })}
-      <h3 className="subcat-title">{ReactConcepts[0].subcategory.name}</h3>
-      {ReactConcepts.map((post) => { 
-        return (
-          <article key={post.id}>
-            <h4 className="post-title">{post.title}</h4>
-            <details> 
-              <summary>Summary</summary> 
-              <StructuredText data={post.summary} /> 
-            </details>
-            <Link href={`/react/${post.slug}`}>
-              <a className="more-link">Read More</a>
-            </Link>
-          </article>
-        )
-      })}
-      <h3 className="subcat-title">{ReactHooks[0].subcategory.name}</h3>
-      {ReactHooks.map((post) => { 
-        return (
-          <article key={post.id}>
-            <h4 className="post-title">{post.title}</h4>
-            <details> 
-              <summary>Summary</summary> 
-              <StructuredText data={post.summary} /> 
-            </details>
-            <Link href={`/react/${post.slug}`}>
-              <a className="more-link">Read More</a>
-            </Link>
-          </article>
-        )
-      })}
-      <h3 className="subcat-title">{ReactDeepDive[0].subcategory.name}</h3>
-      {ReactDeepDive.map((post) => { 
-        return (
-          <article key={post.id}>
-            <h4 className="post-title">{post.title}</h4>
-            <details> 
-              <summary>Summary</summary> 
-              <StructuredText data={post.summary} /> 
-            </details>
-            <Link href={`/react/${post.slug}`}>
-              <a className="more-link">Read More</a>
-            </Link>
-          </article>
-        )
+      {newArr1.map((categoryName) => {
+        const posts = data.allPosts.filter((post) => post.subcategory.name === categoryName )
+        return <>
+          <h3 className="subcat-title">{categoryName}</h3>
+          {posts.map((post) => { 
+            return (
+              <article key={post.id}>
+                <h4 className="post-title">{post.title}</h4>
+                <details> 
+                  <summary>Summary</summary> 
+                  <StructuredText data={post.summary} /> 
+                </details>
+                <Link href={`/react/${post.slug}`}>
+                  <a className="more-link">Read More</a>
+                </Link>
+              </article>
+            )
+          })}
+        </>
       })}
     </Layout>
   )
