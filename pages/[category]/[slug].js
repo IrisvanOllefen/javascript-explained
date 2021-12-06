10db6ebec6162f579f5e21d5da6fa5f00b7f38c7
Iris van Ollefen <irisvanollefen@gmail.com>
2021-12-06 11:05:27 +0100

    Added images in structured text block

--- pages/[category]/[slug].js
+++ pages/[category]/[slug].js
@@ -1,4 +1,6 @@
 import { StructuredText } from 'react-datocms'
+import Image from 'next/image'
+
 import Layout from '../../components/applayout'
 import { request } from '../../lib/datocms'
 import styles from '../../styles/post.module.css'
@@ -15,6 +17,18 @@ query NextPost {
         slug
         content {
             value
+            blocks {
+              __typename
+              ... on ImageBlockRecord {
+                id
+                image { 
+                  url
+                  alt
+                  width
+                  height
+                }
+              }
+            }
         }
     }
 }
@@ -59,7 +73,24 @@ export default function Post({ data }) {
     <Layout categories={data.allCategories}>
       <h2 className={styles['main-post-title']}>{data.post.title}</h2>
       <div className={styles['post-wrapper']}>
-        <StructuredText data={data.post.content.value} />
+        <StructuredText
+          data={data.post.content}
+          renderBlock={({ record }) => {
+            switch (record.__typename) {
+              case 'ImageBlockRecord':
+                return (
+                  <Image
+                    src={record.image.url}
+                    alt={record.image.alt}
+                    width={record.image.width}
+                    height={record.image.height}
+                  />
+                )
+              default:
+                return null
+            }
+          }}
+        />
       </div>
     </Layout>
   )
